8ef098768be1f8110ed464d5535cfd1280bbdf16
isi-code <irvinjose.silva@gmail.com>
2025-10-15 16:31:05 -0700

    Add button to set grid number

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const gridContainer = document.querySelector(".container")
+const button = document.createElement("button");
+button.textContent = "Add grid number";
+document.body.appendChild(button);
 
-for (let g = 0; g < 16; ++g){
-    let div = document.createElement("div");
-    div.setAttribute("class","grid");
-    gridContainer.appendChild(div);
-}
+button.addEventListener("click", () => {
+
+    let number = parseInt(prompt("Grid Number: "));
+    document.body.appendChild(gridContainer);
+
+    for (let g = 0; g < number; ++g) {
+        let div = document.createElement("div");
+        div.setAttribute("class","grid");
+        gridContainer.appendChild(div);
+    }
+
+})
+
+const gridContainer = document.createElement("div");
+gridContainer.setAttribute("class","container");
 
 gridContainer.addEventListener("mouseover", (e) => {
     if(e.target.style.backgroundColor === "" ||e.target.style.backgroundColor === "white"){
