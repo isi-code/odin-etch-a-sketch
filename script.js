a2d486fa05eaf5f0a3492ef1c04164a6086b3db5
isi-code <irvinjose.silva@gmail.com>
2025-10-15 16:16:15 -0700

    Add mouse hover effect
    Change element color to  blue when hovered

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const gridContainer = document.querySelector(".container")
 
 for (let g = 0; g < 16; ++g){
     let div = document.createElement("div");
-    div.setAttribute("class","grid")
-    gridContainer.appendChild(div)
+    div.setAttribute("class","grid");
+    gridContainer.appendChild(div);
 }
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if(e.target.style.backgroundColor === "" ||e.target.style.backgroundColor === "white"){
+        e.target.setAttribute("style", "background-color: blue;")
+    }else{
+        e.target.setAttribute("style", "")
+    }
+});
